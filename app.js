fa4e5308cd4a73e0ac1f78b59f07b3630bbcfea4
Clancy-W <Clancy-W@users.noreply.github.com>
2018-09-25 09:24:39 +1000

    second

--- app.js
+++ app.js
@@ -51,25 +51,42 @@ app.post('/flip', requestVerifier, function(req, res) {
     res.json({
       "version": "1.0",
       "response": {
-        "shouldEndSession": true,
+        "shouldEndSession": false,
         "outputSpeech": {
           "type": "SSML",
-          "ssml": "<speak>Welcome to magic coin <break time=\"0.5s\"/> your decsion making tool</speak>"
+          "ssml": "<speak>Welcome to magic coin <break time=\"0.5s\"/> your decision making tool</speak>"
         }
       }
     });
   }
-  else {
+  else if (req.body.request.type === 'IntentRequest' && req.body.request.intent.name === 'FlipCoin'){
     if (Math.random() > 0.5) {
       a = "heads";
     }
     res.json({
       "version": "1.0",
       "response": {
-        "shouldEndSession": true,
+        "shouldEndSession": false,
         "outputSpeech": {
           "type": "SSML",
-          "ssml": "<speak>You flipped a <break time=\"1s\"/> "+ a +"</speak>"
+          "ssml": "<speak>You flipped a "+ a +"</speak>"
+        }
+      }
+    });
+  }
+  else if (req.body.request.type === 'IntentRequest' && req.body.request.intent.name === 'RollDice'){
+    var sides = 6
+    if (!(!req.body.request.intent.slots.sides ||
+        !req.body.request.intent.slots.sides.value)) {
+      sides = req.body.request.intent.slots.sides.value
+    }
+    res.json({
+      "version": "1.0",
+      "response": {
+        "shouldEndSession": false,
+        "outputSpeech": {
+          "type": "SSML",
+          "ssml": "<speak>You rolled a "+ Math.floor(Math.random() * sides + 1).toString() +" on a " + sides.toString()+" sided dice</speak>"
         }
       }
     });
