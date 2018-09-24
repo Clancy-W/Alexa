db7a0f48fd1ee18ef94ff1d7d0d365464462a17f
Clancy-W <Clancy-W@users.noreply.github.com>
2018-09-24 19:22:13 +1000

    first

---
+++ app.js
@@ -0,0 +1,45 @@
+let express = require('express'),
+  bodyParser = require('body-parser'),
+  app = express();
+
+let alexaVerifier = require('alexa-verifier'); // at the top of our file
+
+function requestVerifier(req, res, next) {
+  alexaVerifier(
+    req.headers.signaturecertchainurl,
+    req.headers.signature,
+    req.rawBody,
+    function verificationCallback(err) {
+      if (err) {
+        res.status(401).json({
+          message: 'Verification Failure',
+          error: err
+        });
+      } else {
+        next();
+      }
+    }
+  );
+}
+
+
+app.use(bodyParser.json({
+  verify: function getRawBody(req, res, buf) {
+    req.rawBody = buf.toString();
+  }
+}));
+app.post('/flip', requestVerifier, function(req, res) {
+  if (req.body.request.type === 'LaunchRequest') {
+    res.json({
+      "version": "1.0",
+      "response": {
+        "shouldEndSession": true,
+        "outputSpeech": {
+          "type": "SSML",
+          "ssml": "<speak>Hmm <break time=\"1s\"/> What day do you want to know about?</speak>"
+        }
+      }
+    });
+  }
+});
+app.listen(3000);
