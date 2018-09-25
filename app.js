1cf77ca4a26e1ba113960a51c61dc6afa6a71a4b
Clancy-W <Clancy-W@users.noreply.github.com>
2018-09-25 15:42:37 +1000

    Added quotes

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let express = require('express'),
   app = express();
 
 let alexaVerifier = require('alexa-verifier'); // at the top of our file
-var obj = require("./inspiration.json");
+var obj = require("inspiration.json");
 
 function requestVerifier(req, res, next) {
   alexaVerifier(
@@ -68,7 +68,7 @@ app.post('/quote', requestVerifier, function(req, res) {
         "shouldEndSession": false,
         "outputSpeech": {
           "type": "SSML",
-          "ssml": "<speak>" + obj["quotes"][0] + "</speak>"
+          "ssml": "<speak>" + obj.quotes[0] + "</speak>"
         }
       }
     });
