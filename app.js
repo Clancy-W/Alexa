fada7a601921040ab8ab5b048aad07d7f5aa323c
Clancy-W <Clancy-W@users.noreply.github.com>
2018-09-25 17:19:06 +1000

    Added yesterday and tomorrow

--- app.js
+++ app.js
@@ -136,4 +136,116 @@ app.post('/quote', requestVerifier, function(req, res) {
       }
     });
   }
+});
+
+app.post('/flip', requestVerifier, function(req, res) {
+  var a = "tails";
+  if (req.body.request.type === 'LaunchRequest') {
+    res.json({
+      "version": "1.0",
+      "response": {
+        "shouldEndSession": false,
+        "outputSpeech": {
+          "type": "SSML",
+          "ssml": "<speak>Welcome to magic coin <break time=\"0.5s\"/> your decision making tool</speak>"
+        }
+      }
+    });
+  }
+  else if (req.body.request.type === 'IntentRequest' && req.body.request.intent.name === 'FlipCoin'){
+    if (Math.random() > 0.5) {
+      a = "heads";
+    }
+    res.json({
+      "version": "1.0",
+      "response": {
+        "shouldEndSession": false,
+        "outputSpeech": {
+          "type": "SSML",
+          "ssml": "<speak>You flipped a "+ a +"</speak>"
+        }
+      }
+    });
+  }
+  else if (req.body.request.type === 'IntentRequest' && req.body.request.intent.name === 'RollDice'){
+    var sides = 6;
+    if (!(!req.body.request.intent.slots.sides ||
+        !req.body.request.intent.slots.sides.value)) {
+      sides = parseInt(req.body.request.intent.slots.sides.value);
+    }
+    console.log(req.body.request.intent.slots.sides);
+    res.json({
+      "version": "1.0",
+      "response": {
+        "shouldEndSession": false,
+        "outputSpeech": {
+          "type": "SSML",
+          "ssml": "<speak>You rolled a "+ Math.floor(Math.random() * sides + 1).toString() +" on a " + sides.toString()+" sided dice</speak>"
+        }
+      }
+    });
+  }
+  else if (req.body.request.type === 'IntentRequest' && req.body.request.intent.name === 'FlipMultiCoin'){
+    var times = 1;
+    if (!(!req.body.request.intent.slots.num ||
+        !req.body.request.intent.slots.num.value)) {
+      times = parseInt(req.body.request.intent.slots.num.value);
+    }
+    var heads = 0
+    for (var i = 0; i < times; i++) {
+      if (Math.random() > 0.5) {
+        heads++;
+      }
+    }
+    res.json({
+      "version": "1.0",
+      "response": {
+        "shouldEndSession": false,
+        "outputSpeech": {
+          "type": "SSML",
+          "ssml": "<speak>After flipping "+ times.toString() + " coins, " + heads.toString() + " of them were heads, and " + (times-heads).toString() + " of them were tails</speak>"
+        }
+      }
+    });
+  }
+  else if (req.body.request.type === 'IntentRequest' && req.body.request.intent.name === 'RollMultiDice'){
+    var sides = 6;
+    if (!(!req.body.request.intent.slots.sides ||
+        !req.body.request.intent.slots.sides.value)) {
+      sides = parseInt(req.body.request.intent.slots.sides.value);
+    }
+    var num = 2;
+    if (!(!req.body.request.intent.slots.num ||
+        !req.body.request.intent.slots.num.value)) {
+      num = parseInt(req.body.request.intent.slots.num.value);
+    }
+    var sum = 0;
+    for (var i = 0; i < times; i++) {
+      if (Math.random() > 0.5) {
+        sum += Math.floor(Math.random() * sides + 1).toString();
+      }
+    }
+    res.json({
+      "version": "1.0",
+      "response": {
+        "shouldEndSession": false,
+        "outputSpeech": {
+          "type": "SSML",
+          "ssml": "<speak>After rolling "+ num.toString() + " die, the sum of them were" + sum.toString() + "</speak>"
+        }
+      }
+    });
+  }
+  else {
+    res.json({
+      "version": "1.0",
+      "response": {
+        "shouldEndSession": true,
+        "outputSpeech": {
+          "type": "SSML",
+          "ssml": "<speak>Goodbye</speak>"
+        }
+      }
+    });
+  }
 }); app.listen(app.get("port"));
